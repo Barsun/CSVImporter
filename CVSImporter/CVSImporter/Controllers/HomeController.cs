925fb7801b5ab577d5f25cb32a548fc084dfb01e
Denis <baciudenis@yahoo.com>
2016-09-13 20:51:36 +0100

    add projetc to git

---
+++ CVSImporter/CVSImporter/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVSImporter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile()
+        {
+            //open file
+            if (Request.Files.Count == 1)
+            {
+                //get file
+                var postedFile = Request.Files[0];
+                if (postedFile.ContentLength > 0)
+                {
+                    //read data from input stream
+                    using (var csvReader = new System.IO.StreamReader(postedFile.InputStream))
+                    {
+                        string inputLine = "";
+
+                        //read each line
+                        while ((inputLine = csvReader.ReadLine()) != null)
+                        {
+                            //get lines values
+                            string[] values = inputLine.Split(new char[] { ',' });
+
+                            for (int x = 0; x < values.Length; x++)
+                            {
+                                //do something with each line and split value
+                            }
+                        }
+
+                        csvReader.Close();
+                    }
+                }
+            }
+
+            return Redirect("/home/uploadfilepage");
+        }
+    }
+}
---
+++ CVSImporter/CVSImporter/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CVSImporter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CVSImporter")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca9ad191-4534-4999-957b-b9a7b68117c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
